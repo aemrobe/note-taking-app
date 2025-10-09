e50e375c2dc12502b2fd29d72fdaf6965d20300a
aemrobe <aemrobekalu083@gmail.com>
2025-10-09 14:48:30 +0300

    make the search box background transparent in a desktop view

--- src/Components/forms/SearchInputBox.jsx
+++ src/Components/forms/SearchInputBox.jsx
@@ -9,7 +9,7 @@ function SearchInputBox({
     <span
       className={`${
         actualSearchQueryFromURL.length > 0 ? "mb-4 xl:mb-0" : ""
-      } flex space-x-2 p-4 xl:py-3 2x:px-4 border border-search-input-border bg-search-input-background rounded-lg mt-4 xl:mt-0 xl:w-[18.75rem]`}
+      } flex space-x-2 p-4 xl:py-3 2x:px-4 border border-search-input-border bg-search-input-background xl:bg-transparent rounded-lg mt-4 xl:mt-0 xl:w-[18.75rem]`}
     >
       <label htmlFor="search" className="text-search-icon">
         <SearchIcon width={"w-5"} />
